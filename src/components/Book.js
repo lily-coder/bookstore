acc1951e82552cbe3c040ab111b82502085e4d98
lily-coder <lilyondie@gmail.com>
2021-10-07 16:41:50 +0300

    Add bookInfo and Book Crud methods

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,26 @@
 /* eslint no-unused-vars: 0 no-undef: 0 */
-import React from 'react';
 
-export default function Book() {
+import PropTypes from 'prop-types';
+
+const Book = (props) => {
+  const { bookInfo } = props;
+  const { category, title, author } = bookInfo;
+
   return (
     <div>
+      <p>{category}</p>
+      <p>{title}</p>
+      <p>{author}</p>
     </div>
   );
-}
+};
+
+Book.defaultProps = {
+  bookInfo: null,
+};
+
+Book.propTypes = {
+  bookInfo: PropTypes.shape,
+};
+
+export default Book;
---
+++ src/components/BookOperations.js
@@ -0,0 +1,9 @@
+const BookOperations = () => (
+  <div>
+    <p>Comments</p>
+    <p>Remove</p>
+    <p>Edit</p>
+  </div>
+);
+
+export default BookOperations;
